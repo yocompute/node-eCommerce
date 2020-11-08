39e172ac66ef70d0dd532af4e15865844c9bc05c
Ling Chen <ling.duke@gmail.com>
2020-11-08 18:18:22 -0500

    tried to solve issue #5

--- src/server.ts
+++ src/server.ts
@@ -12,11 +12,8 @@ import { cfg } from "./config";
 // import { EventRoute } from './routes/event';
 
 import "reflect-metadata";
-import { Connection, createConnection } from "typeorm";
 
-import { AuthRoute } from "./auth/auth.route";
-import { UserRoute } from "./user/user.route";
-import { ProductRoute } from "./routes/product";
+import db_connection from "./db";
 
 dotenv.config();
 
@@ -31,76 +28,10 @@ app.use(bodyParser.json({ limit: "1mb" }));
 
 // the url for the EventSource
 // app.get('/events', sse.init);
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_NAME = process.env.DB_NAME;
-const DB_AUTH_SOURCE = process.env.DB_AUTH_SOURCE;
-const DB_AUTH = process.env.DB_AUTH;
 
-if (DB_AUTH) {
-  createConnection({
-    type: "mongodb",
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    authSource: DB_AUTH_SOURCE,
-    useUnifiedTopology: true,
-    poolSize: 5,
-    entities: [__dirname + "/*/*.entity.js"],
-  })
-    .then((connection: Connection) => {
-      app.use(SVC_PATH + "/auth", AuthRoute(connection));
-      app.use(SVC_PATH + "/users", UserRoute(connection));
-      app.use(SVC_PATH + "/products", ProductRoute(connection));
-      // console.log("Inserting a new user into the database...");
-      // const user = new User();
-      // // user.firstName = "Timber";
-      // // user.lastName = "Saw";
-      // // user.age = 25;
-      // await connection.manager.save(user);
-      // console.log("Saved a new user with id: " + user.id);
 
-      // console.log("Loading users from the database...");
-      // const users = await connection.manager.find(User);
-      // console.log("Loaded users: ", users);
+db_connection()
 
-      // console.log("Here you can setup and run express/koa/any other framework.");
-    })
-    .catch((error) => console.log(error));
-} else {
-  //Todo: No Auth needed mongoDB connection
-  createConnection({
-    type: "mongodb",
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    authSource: DB_AUTH_SOURCE,
-    useUnifiedTopology: true,
-    poolSize: 5,
-    entities: [__dirname + "/*/*.entity.js"],
-  })
-    .then((connection: Connection) => {
-      app.use(SVC_PATH + "/auth", AuthRoute(connection));
-      app.use(SVC_PATH + "/users", UserRoute(connection));
-      app.use(SVC_PATH + "/products", ProductRoute(connection));
-    })
-    .catch((error) => console.log(error));
-}
-
-// const db = new DataBase();
-// const DB_USERNAME: any = process.env.DB_USERNAME;
-// const DB_PASSWORD: any = process.env.DB_PASSWORD;
-// const DB_NAME: any = process.env.DB_NAME;
-
-// db.connect(
-//     DB_USERNAME,
-//     DB_PASSWORD,
-//     cfg.DB_PORT,
-//     DB_NAME,
-// ).then(() => {
-//     app.use(SVC_PATH + "/products", ProductRoute(db));
-//     app.use(SVC_PATH + "/payments", PaymentRoute(db, sse));
-// });
 
 app.listen(cfg.SVC_PORT, () => {
   console.log(`svc path: ${SVC_PATH}`);
