b3d175262224973e06a6adb75711b43ff84dcacd
zlkca <likzhang@gmail.com>
2020-12-06 13:46:23 -0500

    add payment module

---
+++ src/payment/payment.entity.ts
@@ -0,0 +1,23 @@
+import mongoose from '../db';
+
+const { Schema, Types } = mongoose;
+
+const PaymentItemSchema = new Schema({
+    product: { type: Schema.Types.ObjectId, ref: 'Product' },
+    brand: { type: Schema.Types.ObjectId, ref: 'Brand' },
+    quantity: Number
+})
+
+const PaymentSchema = new Schema({
+    items: [PaymentItemSchema],
+    note: String,
+    total: Number,
+    cost: Number,
+    status: String,
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    createUTC: {type: Date, default: new Date()},
+    updateUTC: Date,
+})
+
+export const Payment = mongoose.model('Payment', PaymentSchema, 'payments');
+
